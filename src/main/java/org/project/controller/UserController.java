5222ff4fb947af0c2a6a80b58876bc993c7f3bae
PhuNguyen12345 <phukak12345@gmail.com>
2025-06-07 16:44:02 +0700

    Added frontend and manual access for dashboards

--- src/main/java/org/project/controller/UserController.java
+++ src/main/java/org/project/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -17,4 +18,23 @@ public class UserController {
     public String hello(Model model) {
         return "frontend/product-home";
     }
+    //mapping for admin's dashboard 
+    @GetMapping("/admin/dashboard") 
+    public ModelAndView adminDashboard() {
+		ModelAndView mv = new ModelAndView("dashboard/index"); 
+		return mv;
+	}
+    //mapping for patient's dashboard 
+    @GetMapping("/patient/dashboard") 
+    public ModelAndView patientDashboard() {
+    	ModelAndView mv = new ModelAndView("dashboard/patient-dashboard"); 
+    	return mv; 
+    }
+    //mapping for admin's crud of product 
+    @GetMapping("/admin/product") 
+    public ModelAndView adminProduct() {
+		ModelAndView mv = new ModelAndView("dashboard/products");
+		return mv;
+	}
+    
 }
