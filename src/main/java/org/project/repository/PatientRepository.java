a8d535a9468e8399171f5622784d4266c3b85be6
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-06-10 00:09:40 +0700

    add field PatientRepository

--- src/main/java/org/project/repository/PatientRepository.java
+++ src/main/java/org/project/repository/PatientRepository.java
@@ -1,6 +1,9 @@
 package org.project.repository;
 
+import org.project.entity.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PatientRepository extends JpaRepository {
+@Repository
+public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
 }
