4bf9f71073188c9348559bb453a54cee5f7c9dc6
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-06-09 19:21:28 +0700

    delete old entities

--- src/main/java/org/project/service/impl/PharmacyServiceImpl.java
+++ src/main/java/org/project/service/impl/PharmacyServiceImpl.java
@@ -1,8 +1,6 @@
 package org.project.service.impl;
 
-import lombok.RequiredArgsConstructor;
 import org.project.converter.ConverterPharmacyProduct;
-import org.project.entity.PharmacyProductEntity;
 import org.project.model.response.PharmacyListResponse;
 import org.project.repository.PharmacyRepository;
 import org.project.service.PharmacyService;
