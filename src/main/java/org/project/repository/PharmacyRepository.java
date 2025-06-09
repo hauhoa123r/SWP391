c4719ccf579f62c7da7d97bcb32ff747b3bf791f
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-06-09 21:22:23 +0700

    Reset project

--- src/main/java/org/project/repository/PharmacyRepository.java
+++
@@ -1,8 +0,0 @@
-package org.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PharmacyRepository extends JpaRepository<PharmacyProductEntity, Long> {
-
-}
