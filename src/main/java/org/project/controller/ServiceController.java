d9de44b1abe6dbfd43acb98c07764b969db727c8
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-06-10 22:08:10 +0700

    Create ServiceEntity and related files

---
+++ src/main/java/org/project/controller/ServiceController.java
@@ -0,0 +1,47 @@
+package org.project.controller;
+
+import org.project.model.response.ProductRespsonse;
+import org.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/service")
+public class ServiceController {
+
+    private final int PAGE_SIZE = 9;
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/page/{pageIndex}")
+    public String service(@PathVariable int pageIndex, Model model) {
+        Page<ProductRespsonse> productRespsonsePage = productService.getAllServicesByPage(pageIndex, PAGE_SIZE);
+        if (pageIndex < 0 || pageIndex >= productRespsonsePage.getTotalPages()) {
+            return "frontend/404";
+        }
+        model.addAttribute("services", productRespsonsePage.getContent());
+        model.addAttribute("currentPage", pageIndex);
+        model.addAttribute("totalPages", productRespsonsePage.getTotalPages());
+        return "frontend/service";
+    }
+
+    @GetMapping("/detail/{productId}")
+    public String serviceDetail(@PathVariable Long productId, Model model) {
+        if (!productService.isServiceExist(productId)) {
+            return "frontend/404";
+        }
+        ProductRespsonse productRespsonse = productService.getServiceByProductId(productId);
+        model.addAttribute("service", productRespsonse);
+        return "frontend/service-detail";
+    }
+}
--- src/main/java/org/project/repository/ProductRepository.java
+++ src/main/java/org/project/repository/ProductRepository.java
@@ -3,14 +3,20 @@ package org.project.repository;
 import org.project.entity.ProductEntity;
 import org.project.enums.ProductStatus;
 import org.project.enums.ProductType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findAllByProductTypeAndProductStatus(ProductType productType, ProductStatus productStatus);
 
-    List<ProductEntity> findAllByProductType(ProductType productType);
+    Page<ProductEntity> findAllByProductType(ProductType productType, Pageable pageable);
+
+    ProductEntity findByProductTypeAndId(ProductType productType, Long id);
+
+    boolean existsByProductTypeAndId(ProductType productType, Long id);
 }
