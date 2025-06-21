9a6b18602cd149f5da8d758e9aa8b04e7fcd7ed7
minhvu16 <minhvu160105@gmail.com>
2025-06-21 08:26:17 +0700

    add PharmacyService ( new function to find related product )

--- src/main/java/org/project/service/PharmacyService.java
+++ src/main/java/org/project/service/PharmacyService.java
@@ -2,7 +2,7 @@ package org.project.service;
 
 import org.project.entity.ProductEntity;
 import org.project.enums.ProductSortType;
-import org.project.model.response.PharmacyListResponse;
+import org.project.model.response.PharmacyResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,15 +11,18 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface PharmacyService {
-    List<PharmacyListResponse> findTop10Products();
+    List<PharmacyResponse> findTop10Products();
 
-    PharmacyListResponse findById(Long id);
-//    List<PharmacyListResponse> getAllPharmacies();
-//    Page<PharmacyListResponse> getAllProducts(Pageable pageable, ProductSortType sortType);
-//    Page<PharmacyListResponse> searchByName(String name, Pageable pageable, ProductSortType sortType);
-//    List<PharmacyListResponse> sortProducts(List<PharmacyListResponse> dtos, ProductSortType sortType);
-//    Page<PharmacyListResponse> searchByCategory(Long categoryId, Pageable pageable , ProductSortType sortType);
-//    Page<PharmacyListResponse> searchByPriceRange(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
-//    Page<PharmacyListResponse> searchByType(String type, Pageable pageable);
+    PharmacyResponse findById(Long id);
+    
+    List<PharmacyResponse> findRelatedProducts(Long productId, int limit);
+    Long findFirstProductId();
+    // List<PharmacyListResponse> getAllPharmacies();
+    // Page<PharmacyListResponse> getAllProducts(Pageable pageable, ProductSortType sortType);
+    // Page<PharmacyListResponse> searchByName(String name, Pageable pageable, ProductSortType sortType);
+    // List<PharmacyListResponse> sortProducts(List<PharmacyListResponse> dtos, ProductSortType sortType);
+    // Page<PharmacyListResponse> searchByCategory(Long categoryId, Pageable pageable , ProductSortType sortType);
+    // Page<PharmacyListResponse> searchByPriceRange(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
+    // Page<PharmacyListResponse> searchByType(String type, Pageable pageable);
 
 }
