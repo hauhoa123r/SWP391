a72372b2d10d3b977b78c4cb368f3242d1a50f72
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-06-21 16:31:42 +0700

    Implement DoctorAPI: add pagination and search functionality for doctors by hospital and department

--- src/main/java/org/project/api/DoctorAPI.java
+++ src/main/java/org/project/api/DoctorAPI.java
@@ -1,54 +1,56 @@
 package org.project.api;
 
-import org.project.converter.StaffConverter;
-import org.project.entity.StaffEntity;
-import org.project.enums.Operation;
 import org.project.enums.StaffRole;
-import org.project.model.dto.StaffDTO;
-import org.project.model.response.StaffResponse;
-import org.project.repository.StaffRepository;
-import org.project.utils.filter.FilterSpecification;
-import org.project.utils.filter.SearchCriteria;
+import org.project.model.response.DoctorResponse;
+import org.project.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/doctor")
 public class DoctorAPI {
     private final StaffRole DOCTOR_ROLE = StaffRole.DOCTOR;
+    private final int PAGE_SIZE = 4;
 
-    private FilterSpecification<StaffEntity> filterSpecification;
-    private StaffRepository staffRepository;
-    private StaffConverter staffConverter;
-
-    @Autowired
-    public void setFilterSpecification(FilterSpecification<StaffEntity> filterSpecification) {
-        this.filterSpecification = filterSpecification;
-    }
+    private DoctorService doctorService;
 
     @Autowired
-    public void setStaffRepository(StaffRepository staffRepository) {
-        this.staffRepository = staffRepository;
+    public void setDoctorService(DoctorService doctorService) {
+        this.doctorService = doctorService;
     }
 
-    @Autowired
-    public void setStaffConverter(StaffConverter staffConverter) {
-        this.staffConverter = staffConverter;
+    @GetMapping("/page/{pageIndex}/hospital/{hospitalId}/department/{departmentId}")
+    public ResponseEntity<Map<String, Object>> getDoctorsByHospitalAndDepartment(@PathVariable int pageIndex, @PathVariable Long hospitalId, @PathVariable Long departmentId) {
+        Page<DoctorResponse> doctorResponsePage = doctorService.getAllByHospitalAndDepartment(hospitalId, departmentId, pageIndex, PAGE_SIZE);
+        return ResponseEntity.ok(
+                Map.of(
+                        "doctors", doctorResponsePage.getContent(),
+                        "totalPages", doctorResponsePage.getTotalPages(),
+                        "currentPage", doctorResponsePage.getNumber()
+                )
+        );
     }
 
-    @GetMapping
-    public List<StaffResponse> getDoctors(@RequestBody StaffDTO staffDTO) {
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("staffRole", Operation.EQUALS, DOCTOR_ROLE), Operation.AND);
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("fullName", Operation.CONTAINS, staffDTO.getFullName()), Operation.AND);
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("departmentEntity.name", Operation.EQUALS, staffDTO.getDepartmentEntityName()), Operation.AND);
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("hospitalEntity.name", Operation.EQUALS, staffDTO.getHospitalEntityName()), Operation.AND);
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("reviewEntities.rating", Operation.AVG_EQUALS, staffDTO.getAverageRating()), Operation.AND);
-        filterSpecification = filterSpecification.addSearchCriteria(new SearchCriteria("reviewEntities.id", Operation.COUNT_GREATER_THAN_OR_EQUAL_TO, staffDTO.getReviewCount()), Operation.AND);
-        return staffRepository.findAll(filterSpecification.getSpecification()).stream().map(staffConverter::toResponse).toList();
+    @GetMapping("/page/{pageIndex}/hospital/{hospitalId}/department/{departmentId}/search/{keyword}")
+    public ResponseEntity<Map<String, Object>> searchDoctorsByHospitalAndDepartmentAndKeyword(
+            @PathVariable int pageIndex,
+            @PathVariable Long hospitalId,
+            @PathVariable Long departmentId,
+            @PathVariable String keyword) {
+        Page<DoctorResponse> doctorResponsePage = doctorService.searchAllByHospitalAndDepartmentAndKeyword(hospitalId, departmentId, keyword, pageIndex, PAGE_SIZE);
+        return ResponseEntity.ok(
+                Map.of(
+                        "doctors", doctorResponsePage.getContent(),
+                        "totalPages", doctorResponsePage.getTotalPages(),
+                        "currentPage", doctorResponsePage.getNumber()
+                )
+        );
     }
 }
