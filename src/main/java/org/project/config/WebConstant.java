512bf10c7df6512200331828b888b5784fa73bb4
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-06-23 13:55:04 +0700

    Refactor service queries to filter by active product status and update related methods for clarity

---
+++ src/main/java/org/project/config/WebConstant.java
@@ -0,0 +1,7 @@
+package org.project.config;
+
+import org.project.enums.ProductStatus;
+
+public class WebConstant {
+    public static final ProductStatus PRODUCT_STATUS_ACTIVE = ProductStatus.ACTIVE;
+}
