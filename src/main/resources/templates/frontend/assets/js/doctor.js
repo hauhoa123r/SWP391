42941cb4b587972729d85630ea5c0193940a5f5a
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-07-11 06:43:18 +0700

    Add advanced doctor filtering UI with dynamic search, sort, and department selection

--- src/main/resources/templates/frontend/assets/js/doctor.js
+++ src/main/resources/templates/frontend/assets/js/doctor.js
@@ -1,52 +1,48 @@
-import toast from "/templates/frontend/assets/js/plugins/toast.js";
+import {DoctorDTO} from "/templates/frontend/assets/js/model/dto/DoctorDTO.js";
 import {
-    DepartmentResponse, renderDepartmentResponseForList
+    DepartmentResponse, renderDepartmentResponseForSelect
 } from "/templates/frontend/assets/js/model/response/DepartmentResponse.js";
-import {Pagination} from "/templates/frontend/assets/js/utils/Pagination.js";
 import {
     DoctorResponse, renderDoctorResponseForList
 } from "/templates/frontend/assets/js/model/response/DoctorResponse.js";
-import {fetchData} from "/templates/frontend/assets/js/utils/Fetching.js";
+import toast from "/templates/frontend/assets/js/plugins/toast.js";
+import {FetchingUtils} from "/templates/frontend/assets/js/utils/fetching-utils.js";
+import {FormDataUtils} from "/templates/frontend/assets/js/utils/form-data.js";
+import {Pagination} from "/templates/frontend/assets/js/utils/pagination.js";
+import {SearchParamsUtils} from "/templates/frontend/assets/js/utils/search-params-utils.js";
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 document.addEventListener("DOMContentLoaded", async () => {
-    await renderDepartmentList();
     await renderDoctorList();
+    await renderDepartmentOption();
+    setupFormSubmitEvent();
 });
 
-async function renderDepartmentList(departmentId) {
-    const departmentListElement = $("#department-list");
-    const departmentData = await fetchData("/api/department");
-    const allDepartmentElement = `
-    <button aria-controls="doc-all" aria-selected="true"
-        class="nav-link ${departmentId ? "" : "active"}" type="button">All
-    </button>
+async function renderDepartmentOption(departmentId) {
+    const departmentSelectElement = $("#department-select");
+    if (!departmentSelectElement) return;
+    const departmentData = await FetchingUtils.fetch("/api/department");
+    const defaultOption = `
+        <option value="">Tất cả các khoa</option>
     `;
     if (departmentData) {
-        departmentListElement.innerHTML = allDepartmentElement + DepartmentResponse.fromJsonArray(departmentData).map(department => department.setRenderStrategy(renderDepartmentResponseForList).toHtml(departmentId)).join("");
+        departmentSelectElement.innerHTML = defaultOption + DepartmentResponse.fromJsonArray(departmentData).map(department => department.setRenderStrategy(renderDepartmentResponseForSelect).toHtml(departmentId)).join("");
     }
-    departmentListElement.querySelectorAll("button").forEach(button => {
-        button.addEventListener("click", (event) => {
-            departmentListElement.querySelectorAll("button").forEach(btn => btn.classList.remove("active"));
-            event.currentTarget.classList.add("active");
-            renderDoctorList();
-        });
-    });
 }
 
 async function renderDoctorList(pageIndex = 0) {
     const doctorListElement = $("#doctor-list");
     let url = `/api/doctor/page/${pageIndex}`;
-    const selectedDepartmentElement = $("#department-list .nav-link.active");
-    if (selectedDepartmentElement) {
-        const departmentId = selectedDepartmentElement.dataset.department;
-        if (departmentId) {
-            url += `/department/${departmentId}`;
-        }
+    const filterForm = $("#filter-form");
+    if (filterForm) {
+        const formData = new FormData(filterForm);
+        const doctorDTO = FormDataUtils.getObjectFromFormData(new DoctorDTO(), formData);
+        console.log(doctorDTO);
+        url += "?" + SearchParamsUtils.toSearchParams(doctorDTO);
     }
-    const data = await fetchData(url);
+    const data = await FetchingUtils.fetch(url);
     if (!data) {
         toast.danger("Failed to load doctor data", {
             duration: 3000,
@@ -65,4 +61,17 @@ async function renderDoctorList(pageIndex = 0) {
         paginationElement.innerHTML = pagination.toHtml();
         pagination.setEvent(renderDoctorList);
     }
+}
+
+function setupFormSubmitEvent() {
+    const filterForm = $("#filter-form");
+    if (!filterForm) return;
+    filterForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        await renderDoctorList();
+    });
+    filterForm.addEventListener("reset", async (event) => {
+        await event.target.reset();
+        await renderDoctorList();
+    });
 }
