e0f5e0b8f6a5291defe73c2be7b535ab5259a5d1
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-07-17 01:36:03 +0700

    Feat Paging Approve Result

--- src/main/java/org/project/repository/ResultSampleRepository.java
+++ src/main/java/org/project/repository/ResultSampleRepository.java
@@ -1,7 +1,8 @@
 package org.project.repository;
 
 import org.project.entity.ResultEntity;
+import org.project.repository.impl.ResultSampleRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResultSampleRepository extends JpaRepository<ResultEntity, Long> {
+public interface ResultSampleRepository extends JpaRepository<ResultEntity, Long>, ResultSampleRepositoryCustom {
 }
