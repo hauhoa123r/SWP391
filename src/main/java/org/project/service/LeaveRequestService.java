5591367007cc7cca18ffba983a4820a380decff1
Bui Huy Hung <hunggk2004@gmail.com>
2025-07-21 09:13:20 +0700

    Revert "Get data to leave-staff-list"
    This reverts commit b481da8d3908bed01bb3e4f53887884771d79281.

--- src/main/java/org/project/service/LeaveRequestService.java
+++ src/main/java/org/project/service/LeaveRequestService.java
@@ -6,7 +6,6 @@ import org.project.model.response.LeaveRequestResponse;
 import org.project.model.response.LeaveRequestStatisticResponse;
 import org.springframework.data.domain.Page;
 
-import java.math.BigDecimal;
 import java.time.Year;
 import java.util.List;
 
@@ -22,10 +21,4 @@ public interface LeaveRequestService {
     Page<LeaveRequestResponse> getLeaveRequestByStaffId(Long staffId, int index, int size);
 
     LeaveBalanceResponse getLeaveBalanceByStaffIdAndYear(Long staffId, Year year);
-
-    BigDecimal calculateRequestedDays(LeaveRequestDTO leaveRequestDTO);
-
-    BigDecimal getLeaveDayLeft(LeaveRequestDTO leaveRequestDTO);
-
-    LeaveRequestResponse getLeaveRequestById(Long leaveRequestId);
 }
