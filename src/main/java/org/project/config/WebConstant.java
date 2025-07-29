3adb3fbe683a2337837c6a5502bb1e63f480ca02
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-07-29 09:21:20 +0700

    Add HospitalStatus and DepartmentStatus enums and update WebConstant with new status constants

--- src/main/java/org/project/config/WebConstant.java
+++ src/main/java/org/project/config/WebConstant.java
@@ -1,11 +1,16 @@
 package org.project.config;
 
-import org.project.enums.PatientStatus;
-import org.project.enums.ProductStatus;
-import org.project.enums.UserStatus;
+import org.project.enums.*;
 
 public class WebConstant {
     public static final UserStatus USER_STATUS_ACTIVE = UserStatus.ACTIVE;
     public static final ProductStatus PRODUCT_STATUS_ACTIVE = ProductStatus.ACTIVE;
+    public static final ProductStatus PRODUCT_STATUS_INACTIVE = ProductStatus.INACTIVE;
     public static final PatientStatus PATIENT_STATUS_ACTIVE = PatientStatus.ACTIVE;
+    public static final StaffStatus STAFF_STATUS_ACTIVE = StaffStatus.ACTIVE;
+    public static final StaffStatus STAFF_STATUS_INACTIVE = StaffStatus.INACTIVE;
+    public static final HospitalStatus HOSPITAL_STATUS_ACTIVE = HospitalStatus.ACTIVE;
+    public static final HospitalStatus HOSPITAL_STATUS_INACTIVE = HospitalStatus.INACTIVE;
+    public static final DepartmentStatus DEPARTMENT_STATUS_ACTIVE = DepartmentStatus.ACTIVE;
+    public static final DepartmentStatus DEPARTMENT_STATUS_INACTIVE = DepartmentStatus.INACTIVE;
 }
