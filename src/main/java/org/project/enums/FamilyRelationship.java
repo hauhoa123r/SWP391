1277be2127c3cf106ad0da7aa552be52e603ff72
Bui Huy Hung <hunggk2004@gmail.com>
2025-07-30 17:54:34 +0700

    To do (fix enum relasionship). Miss header, footer dashboard-patient

--- src/main/java/org/project/enums/FamilyRelationship.java
+++ src/main/java/org/project/enums/FamilyRelationship.java
@@ -6,21 +6,21 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum FamilyRelationship {
-    SELF("Self"),
-    WIFE("Wife"),
-    HUSBAND("Husband"),
-    FATHER("Father"),
-    MOTHER("Mother"),
-    BROTHER("Brother"),
-    SISTER("Sister"),
-    SON("Son"),
-    DAUGHTER("Daughter"),
-    GRAND_FATHER("Grandfather"),
-    GRAND_MOTHER("Grandmother"),
-    COUSIN("Cousin"),
-    AUNT("Aunt"),
-    UNCLE("Uncle"),
-    OTHER("Other");
+    SELF("Bản thân"),
+    WIFE("Vợ"),
+    HUSBAND("Chồng"),
+    FATHER("Bố"),
+    MOTHER("Mẹ"),
+    BROTHER("Anh / Em trai"),
+    SISTER("Chị / Em gái"),
+    SON("Con trai"),
+    DAUGHTER("Con gái"),
+    GRAND_FATHER("Ông"),
+    GRAND_MOTHER("Bà"),
+    COUSIN("Cháu"),
+    AUNT("Dì"),
+    UNCLE("Chú"),
+    OTHER("Khác");
 
     private final String relationship;
 }
